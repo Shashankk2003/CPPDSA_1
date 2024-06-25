55fed37156a4a939ac724d36872527fe87cd7623
Shashankk2003 <shashankkapoor4115@gmail.com>
2024-06-25 09:50:01 +0530

    Kadane's Algorithm

---
+++ New folder/DSA_11.cpp
@@ -0,0 +1,22 @@
+//kadane's Algorithm
+#include<iostream>
+using namespace std;
+int maxSubArraySum(int arr[],int size){
+    int max=INT_MIN;
+    int sum=0;
+
+    for(int i=0;i<size;i++){
+   sum += arr[i];
+   if(sum>max)max=sum;
+
+   if(sum<0)sum=0;
+}
+return max;
+}
+int main(){
+  int arr[]={-2,-3,4,-1,-2,1,5,-3};
+
+  cout<<maxSubArraySum(arr,8);
+
+  return 0;
+}
